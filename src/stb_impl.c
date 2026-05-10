f12e1a9afaa061751d162fee3850d117b84ebc9a
neguse <neguse@neguse.dev>
2026-05-11 01:31:17 +0900

    build: vendor stb_image.h for PNG decode

--- src/stb_impl.c
+++ src/stb_impl.c
@@ -1,2 +1,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_HDR
+#define STBI_NO_LINEAR
+#define STBI_ONLY_PNG
+#include "stb_image.h"
